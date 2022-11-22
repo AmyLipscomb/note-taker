1bb2c7f427413818e5768fa32c1d2ca01be16353
Amy Lipscomb <amylipscomb@macbook-pro.myfiosgateway.com>
2022-11-22 07:49:42 -0500

    cleaned up comments from server.js file

--- server.js
+++ server.js
@@ -5,36 +5,16 @@ const apiRoutes= require('./routes/apiroutes');
 //For when you send to Heroku. 
 const PORT = process.env.PORT || 9002;
 
+//Creating an 'app' variable set to the value of 'express()'
 const app = express();
-//^Creating an 'app' variable set to the value of 'express()'
 
-//TO DO: INSERT MIDDLEWARE
+
+//Insert MIDDLEWARE
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(apiRoutes);
 app.use(htmlRoutes);
-
-//The following API routes should be created:
-
-//* `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
-
-// app.get('/', (req, res) => {
-//     res.json(db)
-    // res.sendFile("/api/notes")
-// });
-
-//^The route matches the fetch
-
-
-//* `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
-
-// app.post('/',(req, res) => {
-//     res.sendFile("/api/notes")
-// });
-
-
-
 
 
 //Have the app listen on PORT 9002
