8bd262dda9a94040ef4d5a92b30f223c50cfde8e
Amy Lipscomb <amylipscomb@macbook-pro.myfiosgateway.com>
2022-11-22 07:40:37 -0500

    added post router

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -12,6 +12,17 @@ router.get('/api/notes', (req, res) => {
 
 
 
-
+router.post('/api/notes', (req, res) => {
+    const dbData = JSON.parse(fs.readFileSync('db/db.json'));
+    const newNote = req.body;
+    const id ='id';
+    const notesId = uniqid();
+    newNote[id]= notesId;
+    dbData.push(newNote);
+    fs.writeFile('db/db.json',JSON.stringify(dbData),(err)=>{
+        if(err) throw err;
+        res.json(dbData);
+    })
+})
 
 module.exports = router;
